7b278ec18eb6fe964215627331531c4c40638ede
Raghuveer <jraghuveer848@gmail.com>
2019-09-04 21:27:19 +0530

    springboot

--- SpringSchedular/src/main/java/com/novartis/SpringSchedularApplication.java
+++ SpringSchedular/src/main/java/com/novartis/SpringSchedularApplication.java
@@ -9,23 +9,7 @@ public class SpringSchedularApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSchedularApplication.class, args);
 		System.out.println("Application started..");
-		System.out.println("latest========");
-		first();
-		second();
-		third();
+
 	}
 
-	private static void third() {
-		// TODO Auto-generated method stub
-		System.out.println("Third");
-	}
-
-	private static void first() {
-		// TODO Auto-generated method stub
-		System.out.println("First");
-	}
-	private static void second() {
-		// TODO Auto-generated method stub
-		System.out.println("Second");
-	}
 }
